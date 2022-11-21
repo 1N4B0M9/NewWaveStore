cef0ab1603ebb59b6fd4aa3a42f3f5154a2a5e51
Nathan Morelli <2005Nathan2005@gmail.com>
2022-11-21 07:35:35 -0500

    Initial Commit

---
+++ NewWaveStore/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  NewWaveStore
+//
+//  Created by Nathan Morelli (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
